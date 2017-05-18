4642c2c2d26d277353ebb723c0e91e14a5759e6f
Felix <felix.schweikardt@gmx.de>
2017-05-18 09:12:31 +0200

    Dateiimport erfolgreich

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -6,13 +6,20 @@ import numpy as np
 import os
 from scipy.stats import skew
 
-# ###Only apply if default directories are not working###
-#
-# ###Change working directory###
-# os.chdir("C:\\Users\\Felix Schweikardt\\PycharmProjects\\project_bold_eagle_V3")
-#
-# ###check if change of working directory worked###
-# cwd = os.getcwd()
+###Only apply if default directories are not working###
+
+###Change working directory###
+f = "Felix Schweikardt"
+l = ""
+os.chdir("C:\\Users\\"+"Felix Schweikardt"+"\\Dropbox\\Seminararbeit FZI - Softsensor\\Datensätze\\12-05-2017")
+
+###check if change of working directory worked###
+cwd = os.getcwd()
 
 #read the csv files
-training_data = pd.read_csv('train1.csv')
+dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+training_data = pd.read_csv('Muehle_1.csv', parse_dates=['Time'], date_parser=dateparse)
+training_data = training_data.set_index('Time')
+
+training_data = pd.read_csv('Muehle_1.csv', parse_dates=['Time'], date_parser=dateparse)
+training_data = training_data.set_index('Time')
