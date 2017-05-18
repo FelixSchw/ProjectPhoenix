a4c6d11c1639aab5bcc7ad6f88b83826fb2a49c6
leopoldspenner <l.spenner@hotmail.de>
2017-05-18 10:19:28 +0200

    - added UTF-8 encoding - specified Leos path

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #author: Schweikardt & Spenner
 #version: 1.0 May 2017
 
@@ -9,17 +11,14 @@ from scipy.stats import skew
 ###Only apply if default directories are not working###
 
 ###Change working directory###
-f = "Felix Schweikardt"
-l = ""
-os.chdir("C:\\Users\\"+"Felix Schweikardt"+"\\Dropbox\\Seminararbeit FZI - Softsensor\\Datensätze\\12-05-2017")
+f = "C:\\Users\\Felix Schweikardt\\Dropbox\\Seminararbeit FZI - Softsensor\\Datensätze\\12-05-2017"
+l = "/Users/leopoldspenner/Dropbox/Seminararbeit FZI - Softsensor/Datensätze/12-05-201/Fertige_Sets"
+os.chdir(l)
 
 ###check if change of working directory worked###
 cwd = os.getcwd()
 
 #read the csv files
 dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-training_data = pd.read_csv('Muehle_1.csv', parse_dates=['Time'], date_parser=dateparse)
-training_data = training_data.set_index('Time')
-
-training_data = pd.read_csv('Muehle_1.csv', parse_dates=['Time'], date_parser=dateparse)
+training_data = pd.read_csv('Predictors_1.csv', parse_dates=['Time'], date_parser=dateparse)
 training_data = training_data.set_index('Time')
